93062b2f8b05902cc97b8854660c8ea68f9def0d
David Coallier <dcoallier@engineyard.com>
2014-02-10 19:40:08 +0000

    Added jsonify

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Engine Yard Python World!'
+    return jsonify(message="Hello Engine Yard Folks!", where="Straight from Python")
 
 @app.route('/test')
 def test():
-    return "Testing from a route in Python"
+    return jsonify(message="We just tested a route! Boom")
 
 if __name__ == '__main__':
     app.run()
